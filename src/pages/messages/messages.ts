026688f225e875d15a76dad99f59560309a4144b
Noel Hwande <noel.hwande@gmail.com>
2016-12-08 17:45:37 -0600

    Added reatime Meteor server

---
+++ src/pages/messages/messages.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from "@angular/core";
+import { NavParams } from "ionic-angular";
+import { Chat, Message } from "api/models/ndeipi-models";
+import { Messages } from "api/collections/ndeipi-collections";
+import { Observable} from "rxjs";
+
+@Component({
+  selector: "messages-page",
+  templateUrl: "messages.html"
+})
+export class MessagesPage implements OnInit {
+  selectedChat: Chat;
+  title: string;
+  picture: string;
+  messages: Observable<Message[]>;
+
+  constructor(navParams: NavParams) {
+    this.selectedChat = <Chat>navParams.get('chat');
+    this.title = this.selectedChat.title;
+    this.picture = this.selectedChat.picture;
+  }
+
+  ngOnInit() {
+    let isEven = false;
+
+    this.messages = Messages.find(
+      {chatId: this.selectedChat._id},
+      {sort: {createdAt: 1}}
+    ).map((messages: Message[]) => {
+        messages.forEach((message: Message) => {
+          message.ownership = isEven ? 'mine' : 'other';
+          isEven = !isEven;
+        });
+
+        return messages;
+      });
+  }
+}
